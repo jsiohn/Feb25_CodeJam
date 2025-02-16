bcf78e5d9db3db1e6c2fe93741f83acad99178df
John Siordia <siohn@proton.me>
2025-02-16 15:15:13 -0600

    formatting changes to images and text, added submit button, more javascript function

--- script.js
+++ script.js
@@ -3,43 +3,70 @@
 /* -------------------------------------------------------------------------- */
 
 const cardButton = document.querySelectorAll("#radio");
-const optIn = document.querySelector(".content__bubble-image");
+const optIn = document.querySelector(".content__bubble-text");
+const quizSubmit = document.querySelector(".quiz__submit-button");
 
 /* -------------------------------------------------------------------------- */
 /*                                  Variables                                 */
 /* -------------------------------------------------------------------------- */
 
-// variable to store answers as array
-let userAnswers = [];
+let userAnswer = "";
+const apiKey =
+  "ed3841ce233bc6f7d78f21a55fa79295a3a88918WGoIEy7kot5ZWMfYMmJmDB4Fv";
 
 /* -------------------------------------------------------------------------- */
 /*                                  Functions                                 */
 /* -------------------------------------------------------------------------- */
 
-// function to add answer value to array
-function getRadioValue() {
+// function to display result
+function displayResult() {
   const selectedRadio = document.querySelector('input[name="radio"]:checked');
-  if (selectedRadio) {
-    return parseInt(selectedRadio.value);
+  if (!selectedRadio) {
+    alert("Please select an photo");
+    return;
+  }
+  if (selectedRadio.value === "1") {
+    alert("Moody was chosen");
+  }
+  if (selectedRadio.value === "2") {
+    alert("Analytical was chosen");
+  }
+  if (selectedRadio.value === "3") {
+    alert("Cutesy was chosen");
+  }
+  if (selectedRadio.value === "4") {
+    alert("Outdoorsy was chosen");
   }
 }
 
-// function to get sum of user answers
-function calculateSum() {
-  return userAnswers.reduce((total, num) => total + num, 0);
+// function to send SMS
+function sendSMS() {
+  let userPhone = prompt("Please enter your phone number");
+  if (userPhone !== null || "") {
+    fetch("https://textbelt.com/text", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        phone: userPhone,
+        message: "Hello world",
+        key: apiKey,
+      }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  } else {
+    alert("Please enter a valid phone number");
+  }
 }
 
 /* -------------------------------------------------------------------------- */
 /*                                  Event Listeners                           */
 /* -------------------------------------------------------------------------- */
 
-cardButton.forEach((e) => {
-  e.addEventListener("click", () => {
-    userAnswers.push(getRadioValue());
-    console.log(userAnswers);
-  });
-});
+quizSubmit.addEventListener("click", displayResult);
 
-optIn.addEventListener("click", () => {
-  console.log("clicked!");
-});
+optIn.addEventListener("click", sendSMS);
