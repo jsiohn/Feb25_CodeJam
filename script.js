14cc6ebe22d9d4334ed6309092184a6e81c977c2
John Siordia <siohn@proton.me>
2025-02-17 09:47:03 -0600

    last JS work, edit footer style

--- script.js
+++ script.js
@@ -19,28 +19,56 @@ const apiKey =
 /* -------------------------------------------------------------------------- */
 
 // function to display result
-function displayResult() {
+function displayResult(e) {
+  e.preventDefault();
   const selectedRadio = document.querySelector('input[name="radio"]:checked');
   if (!selectedRadio) {
-    alert("Please select an photo");
+    alert("Please select a photo");
     return;
   }
   if (selectedRadio.value === "1") {
-    alert("Moody was chosen");
+    userAnswer = `"Dark Academic, Moody, Edgy, Gothic"
+Based on the answer you provided your significant other's personality best fits into one of the above tropes. While no two are alike they do share some similarities. Here are our suggestions:
+- Vacation: Locations that are rich in gothic architecture, dark vibes, and rich history for example Edinburgh, Scotland.
+- Gift: Limited Edition Hardcover of their favorite book, Vinyl records, antiques, or tickets to their favorite band.
+- Date: Find locations that are cozy, dimly lit, and bonus points if it has a cool backstory. Examples include speakeasys and jazz bars.`;
+    alert(userAnswer);
   }
   if (selectedRadio.value === "2") {
-    alert("Analytical was chosen");
+    userAnswer = `"Logical, Gamer, Tech-Savvy"
+Based on the answer you provided your significant other's personality best fits into one of the above tropes. While no two are alike they do share some similarities. Here are our suggestions:
+- Vacation: Locations that are rich in immersive experiences, technology, and pop-culture for example Tokyo, Japan.
+- Gift: Limited edition lego sets, new gaming gear, or new tech gadgets.
+- Date: Immersive experiences such as SandBox VR.`;
+    alert(userAnswer);
   }
   if (selectedRadio.value === "3") {
-    alert("Cutesy was chosen");
+    userAnswer = `"Whimsical, Charming, Lively"
+Based on the answer you provided your significant other's personality best fits into one of the above tropes. While no two are alike they do share some similarities. Here are our suggestions:
+- Vacation: Locations that are rich in culture, arts, and aesthetics for example Prague, Czech Republic or New Orleans, USA.
+- Gift: Personalized/sentimental gift are best. Examples include personalized playlists or scrapbooks. Anything that would be meaningful while making a good Instagram post.
+- Date: Fun and low-key dates are the best fit for this personality. Examples include walking though the local botanical gardens, museum dates, or going to a jazz bar.`;
+    alert(userAnswer);
   }
   if (selectedRadio.value === "4") {
-    alert("Outdoorsy was chosen");
+    userAnswer = `"Free-Spirited, Thrill-seeker"
+Based on the answer you provided your significant other's personality best fits into one of the above tropes. While no two are alike they do share some similarities. Here are our suggestions:
+- Vacation: Any location that is rich in natural scenery and hiking trails for example Lake Tahoe, USA or Fiordland, NZ.
+- Gift: Items that are adventure-ready or nature-inspired. If they're an avid traveler you can opt for the Go-Wild pass by Frontier airlines for unlimited flying.
+- Date: Scenic Hike, Weekend Camping Trips, or Thrilling new experiences like skydiving or ziplining`;
+    alert(userAnswer);
   }
+  console.log(userAnswer); // debugging
 }
 
 // function to send SMS
 function sendSMS() {
+  if (userAnswer === "") {
+    alert(
+      "Please answer the question to get your significant other's personality"
+    );
+    return;
+  }
   let userPhone = prompt("Please enter your phone number");
   if (userPhone !== null || "") {
     fetch("https://textbelt.com/text", {
@@ -48,7 +76,7 @@ function sendSMS() {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         phone: userPhone,
-        message: "Hello world",
+        message: userAnswer,
         key: apiKey,
       }),
     })
